ea6583daa44782b8fb92479608296c969228bf76
Banu-Liyanage <banuka2002liyanage@gmail.com>
2025-07-30 22:34:50 +0530

    Added closest pair algorithm

---
+++ Algorithms/ClosestPair.java
@@ -0,0 +1,8 @@
+//You are given an array arr[] of n distinct points in a 2D plane,
+// where each point is represented by its (x, y) coordinates. 
+// Find the minimum Euclidean distance between two distinct points.
+
+
+public class ClosestPair {
+   
+}
