7a9fd956030050c5c242e70f14a146049059315b
Banu-Liyanage <banuka2002liyanage@gmail.com>
2025-08-03 23:31:01 +0530

    updated matrix_mul

--- Algorithms/Matrix_Mul.java
+++ Algorithms/Matrix_Mul.java
@@ -1,5 +1,5 @@
 //here i implemented the Strassen's Algorithm for Matrix Multiplication of a square matrix n x n 
-
+//First try to multiply two 2x2 matrices and them use 1x1 as the base case
 public class Matrix_Mul {
     
 }
